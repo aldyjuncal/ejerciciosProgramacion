0b12dbc2f91f9f376e1373f88fb71993b411079f
aldyjuncal <67516977+aldyjuncal@users.noreply.github.com>
2022-08-25 16:24:22 -0300

    subi un archivo

---
+++ 1-instrucciontorta/src/index.ts
@@ -0,0 +1,13 @@
+let torta = [
+  "Torta de Aceite",
+  "1-En un bol Colocas los huevos,el azúcar,el aceite,la leche",
+  "2-Batir a mano pero no mucho",
+  "3-Agregas la vainilla o lo que quieras",
+  "4-Agregar Harina",
+  "5-Hornear por 35/40' a 180°"
+];
+
+for (let i = 0; i < torta.length; i++) {
+  const pasos = torta[i];
+  console.log(pasos);
+}
